a1c95c6db3195ebd24f1a905b438519e8cbab000
mainmeister <mainmmister@gmail.con>
2017-10-24 19:24:35 -0400

    initial commit

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ uses:
 # searchtext.send_keys('byte magazine')
 # searchtext.submit()
 
-from internetarchive import download
 from internetarchive import search_items
 from internetarchive import get_session
 from internetarchive import get_item
 from internetarchive import File
 
+
 s = get_session()
-search_results = search_items("byte_issue")
+search_results = search_items("magazine")
 for i in search_results:
     identifier = i['identifier']
     item = get_item(identifier)
     for file in item.files:
-        format = str(file['format'])
+        format = str(file['format']).lower()
         name = str(file['name'])
         #print name + ' -- ' + format
-        if format == 'Text PDF' and name.lower().find('byte') != -1:
+        if format.find('pdf') != -1: #and name.lower().find('byte') != -1:
             fi = File(item, name)
             try:
                 fi.download(destdir='./download/' , verbose=True)
